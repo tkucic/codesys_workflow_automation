d36ec0d355c1eca216b414f8e44878bb7c588cb1
Toni Kucic <tkucic@gmail.com>
2021-03-10 19:57:39 +0200

    Add first version of doc generating script

--- createDocs.py
+++ createDocs.py
@@ -1,7 +1,7 @@
+#Codesys iron python / python 2.7
 from __future__ import print_function
 from saveAndExport import saveAndExport
-import os, sys
-#import ia_tools
+import os, sys, subprocess
 
 #This script will 
 #Save the project and export the plc open xml to the same path as the .project file
@@ -10,14 +10,34 @@ filePath = saveAndExport(project, '-p')
 
 if filePath != None:
     #Calculate the file path of the docs folder
-    docsPath = os.path.join(os.path.dirname(filePath), 'docs')
+    savePath = os.path.join(os.path.dirname(filePath), 'docs')
 
-    #Set the format to iecMd for the first version
-    format = 'iecMd'
+    #Set the format based on arguments
+    print(sys.argv)
+    try:
+        if sys.argv[1] in ['html', 'iecMd', 'md', 'iec']:
+            format = sys.argv[1]
 
-    #Call an external python script that will generate the documentation (Python3 and ia_tools must be installed)
-    #proj = ia_tools.Project(filepath)
-    print(docsPath, format)
+            #Call an external python script that will generate the documentation (Python3 and ia_tools must be installed)
+            ia_toolsScriptLocation = os.path.join(os.path.dirname(__file__), 'call_ia_tools.py')
+            
+            #Check if the call_ia_tools.py exists
+            if os.path.isfile(ia_toolsScriptLocation):
+                cmd = ['python', 
+                    ia_toolsScriptLocation,
+                    filePath,
+                    savePath,
+                    format]
+                if subprocess.call(cmd) != 0:
+                    print('Doc generation failed, failed to execute call_ia_tools.py')
+                else:
+                    print('Documentation generated succesfully')
+            else:
+                print('Doc generation failed, call_ia_tools.py script not found in ScriptCommands')
+
+    except IndexError:
+        print('Doc generation failed, export format argument missing or unsupported')
+
 else:
     print('Doc generation failed, xml file not generated')
     
