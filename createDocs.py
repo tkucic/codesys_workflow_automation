7a056dff7e6adad63e7bcbd410d6abe941f17a10
Toni Kucic <tkucic@gmail.com>
2021-03-11 21:05:02 +0200

    Add non public generateDocs script

--- createDocs.py
+++ createDocs.py
@@ -1,45 +1,44 @@
 #Codesys iron python / python 2.7
 from __future__ import print_function
-from saveAndExport import saveAndExport
-import os, sys, subprocess
+from interfaceLib import saveAndExport
+import os, subprocess
 
 #This script will 
 #Save the project and export the plc open xml to the same path as the .project file
 project = projects.primary
 filePath = saveAndExport(project, '-p')
 
-if filePath != None:
-    #Calculate the file path of the docs folder
-    savePath = os.path.join(os.path.dirname(filePath), 'docs')
+if filePath == None:
+    raise Exception('Project xml not generated')
 
-    #Set the format based on arguments
-    print(sys.argv)
-    try:
-        if sys.argv[1] in ['html', 'iecMd', 'md', 'iec']:
-            format = sys.argv[1]
+#Calculate the file path of the docs folder
+savePath = os.path.join(os.path.dirname(filePath), 'docs')
 
-            #Call an external python script that will generate the documentation (Python3 and ia_tools must be installed)
-            ia_toolsScriptLocation = os.path.join(os.path.dirname(__file__), 'call_ia_tools.py')
-            
-            #Check if the call_ia_tools.py exists
-            if os.path.isfile(ia_toolsScriptLocation):
-                cmd = ['python', 
-                    ia_toolsScriptLocation,
-                    filePath,
-                    savePath,
-                    format]
-                if subprocess.call(cmd) != 0:
-                    print('Doc generation failed, failed to execute call_ia_tools.py')
-                else:
-                    print('Documentation generated succesfully')
-            else:
-                print('Doc generation failed, call_ia_tools.py script not found in ScriptCommands')
+#Set the format based on config
+#Choose from['html', 'iecMd', 'md', 'iec']
+format = 'iecMd'
 
-    except IndexError:
-        print('Doc generation failed, export format argument missing or unsupported')
-
-else:
-    print('Doc generation failed, xml file not generated')
+if format in ['html', 'iecMd', 'md', 'iec']:
     
+    #Call an external python script that will generate the documentation (Python3 and ia_tools must be installed)
+    ia_toolsScriptLocation = os.path.join(os.path.dirname(__file__), 'call_ia_tools.py')
+    
+    #Check if the call_ia_tools.py exists
+    if os.path.isfile(ia_toolsScriptLocation):
+        cmd = ['python', 
+            ia_toolsScriptLocation,
+            filePath,
+            savePath,
+            format]
+        if subprocess.call(cmd) != 0:
+            raise Exception('Execution of call_ia_tools.py failed')
+        else:
+            print('Documentation generated succesfully')
+    else:
+        raise Exception('Script call_ia_tools.py not installed')
+else:
+    raise Exception('Invalid documentation format')
+
+
 
 
